b58b63137f90dbc83944ca0c292ef2bddfb8d33f
sua01 <sua0211@naver.com>
2022-07-05 15:22:04 +0900

    처음 저장소에 올림

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://sua:abcd0211@boilerplate.h3ed1.mongodb.net/?retryWrites=true&w=majority', {
+  useNewUrlParser : true, useUnifiedTopology: true
+}).then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World! 안녕!!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
