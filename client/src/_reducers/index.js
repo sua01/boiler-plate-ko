315f557b59c0a2ff34cb423b42fc18fa472eaec8
sua01 <sua0211@naver.com>
2022-08-22 18:30:04 +0900

    로그인 전까지

---
+++ client/src/_reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+// ipmort user from './user_reducer';
+
+const rootReducer = combineReducers({
+  // user,
+})
+
+export default rootReducer;
--- client/src/index.js
+++ client/src/index.js
@@ -2,18 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
 import 'antd/dist/antd.css';  // Ant Design
+import { applyMiddleware, createStore } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import Reducer from './_reducers';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+const creatStorewithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk) (createStore) // 객체밖에 못받으니까 promise, functions도 받을 수 있게 middleware 사용
+
+ReactDOM.render(
+  
+  <Provider
+    store={creatStorewithMiddleware(Reducer,
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+
+      )}
+  >
     <App />
-  </React.StrictMode>
-);
+  </Provider>
+
+  , document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+serviceWorker.unregister();
