d48eec12a11c2f91ff082bd03606b570be08ca3f
sua01 <sua0211@naver.com>
2022-09-07 14:13:28 +0900

    29강까지

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
-import 'antd/dist/antd.css';  // Ant Design
+import 'antd/dist/antd.min.css';  // Ant Design
 import { applyMiddleware, createStore } from 'redux';
 import promiseMiddleware from 'redux-promise';
 import ReduxThunk from 'redux-thunk';
